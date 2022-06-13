422590fbe86b0a61b67e876e90d2080283fc4d60
unknown <biminhiabo2@gmail.com>
2022-06-14 01:40:32 +0700

    .

--- ckeditor/ckeditor/src/components/HelloWorld.vue
+++ ckeditor/ckeditor/src/components/HelloWorld.vue
@@ -39,7 +39,11 @@ export default {
     handle_change:function(event){
       //console.log(event)
       this.$emit('update:modelValue', event);
-    }
+    },
+     getId(url) {
+  let regex = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))/gm;
+  return regex.exec(url)[3];
+}
   },
   data() {
     return {
@@ -64,7 +68,52 @@ export default {
             "https://89901.cke-cs.com/token/dev/a3569ae970a35ac828ac570b3251fb87f9ab9d272ed044558d088f28b297?limit=10",
           uploadUrl: "https://89901.cke-cs.com/easyimage/upload/",
         },
+mediaEmbed: {
+                previewsInData: true,
+                removeProviders: ['youtube'],
+                 providers: [
+                {
+                    // hint: this is just for previews. Get actual HTML codes by making API calls from your CMS
+                    name: 'iframely previews', 
 
+                    // Match all URLs or just the ones you need:
+                    url: /.+/,
+
+                        html: match => {
+                            const id =this.getId(match[0]);
+        console.log(match)
+                            return (
+                                '<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
+                                `<iframe src="https://www.youtube.com/embed/${ id }" ` +
+                                'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+                                'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
+                                '</iframe>' +
+                                '</div>'
+                            );
+                        }
+                    },
+                    {
+                        name: 'afreecaTV',
+                        url: [
+                            /^v\.afree\.ca\/ST\/([\w-]+)/,
+                            /^vod\.afreecatv\.com\/([\w-]+)/,
+                            /^play\.afreecatv\.com\/([\w-]+)/,
+                        ],
+                        html: match => {
+                            const id = match[ 1 ];
+
+                            return (
+                                '<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
+                                `<iframe src="https://openapi.afreecatv.com/oembed/embedinfo/${ id }" ` +
+                                'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+                                'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
+                                '</iframe>' +
+                                '</div>'
+                            );
+                        }
+                    },
+                ]
+            },
         toolbar: {
           items: [
             "bold",
--- tinymce/src/App.vue
+++ tinymce/src/App.vue
@@ -1,32 +1,89 @@
- <template>
-   <div id="app">
-     <img alt="Vue logo" src="./assets/logo.png">
-     <editor
-       api-key="7adu91k2pv8qgm8gxdj1uaw6wlb4vgyssjco57kc06qrf90e"
-       :init="{
-         height: 500,
-         menubar: true,
-         plugins: [
-           'advlist autolink lists link image charmap print preview anchor',
-           'searchreplace visualblocks code fullscreen',
-           'insertdatetime media table paste code help wordcount'
-         ],
-         toolbar:
-           'undo redo | formatselect | bold italic backcolor | \
-           alignleft aligncenter alignright alignjustify | \
-           bullist numlist outdent indent | removeformat | help'
-       }"
-     />
-   </div>
- </template>
+<template>
+  <div id="app">
+    <editor
+      api-key="7adu91k2pv8qgm8gxdj1uaw6wlb4vgyssjco57kc06qrf90e"
+      :init="config"
+    />
+  </div>
+</template>
 
- <script>
- import Editor from '@tinymce/tinymce-vue'
+<script>
+import Editor from "@tinymce/tinymce-vue";
+const useDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const isSmallScreen = window.matchMedia("(max-width: 1023.5px)").matches;
+const config = {
+  selector: "textarea#full-featured",
+  plugins:
+    "preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker editimage help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable export",
+  tinydrive_token_provider: "URL_TO_YOUR_TOKEN_PROVIDER",
+  tinydrive_dropbox_app_key: "YOUR_DROPBOX_APP_KEY",
+  tinydrive_google_drive_key: "YOUR_GOOGLE_DRIVE_KEY",
+  tinydrive_google_drive_client_id: "YOUR_GOOGLE_DRIVE_CLIENT_ID",
+  mobile: {
+    plugins:
+      "preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap pagebreak nonbreaking anchor tableofcontents insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker help formatpainter pageembed charmap mentions quickbars linkchecker emoticons advtable",
+  },
+  menu: {
+    tc: {
+      title: "Comments",
+      items: "addcomment showcomments deleteallconversations",
+    },
+  },
+  menubar: "file edit view insert format tools table tc help",
+  toolbar:
+    "undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment",
+  toolbar_sticky: true,
+  toolbar_sticky_offset: isSmallScreen ? 102 : 108,
+  autosave_ask_before_unload: true,
+  autosave_interval: "30s",
+  autosave_prefix: "{path}{query}-{id}-",
+  autosave_restore_when_empty: false,
+  autosave_retention: "2m",
+  image_advtab: true,
+  link_list: [
+    { title: "My page 1", value: "https://www.tiny.cloud" },
+    { title: "My page 2", value: "http://www.moxiecode.com" },
+  ],
+  image_list: [
+    { title: "My page 1", value: "https://www.tiny.cloud" },
+    { title: "My page 2", value: "http://www.moxiecode.com" },
+  ],
+  image_class_list: [
+    { title: "None", value: "" },
+    { title: "Some class", value: "class-name" },
+  ],
 
- export default {
-   name: 'app',
-   components: {
-     'editor': Editor
-   }
- }
- </script>
+  importcss_append: true,
+
+  template_cdate_format: "[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]",
+  template_mdate_format: "[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]",
+  height: 600,
+  image_caption: true,
+  quickbars_selection_toolbar:
+    "bold italic | quicklink h2 h3 blockquote quickimage quicktable",
+  noneditable_class: "mceNonEditable",
+  toolbar_mode: "sliding",
+  spellchecker_ignore_list: ["Ephox", "Moxiecode"],
+  tinycomments_mode: "embedded",
+  content_style: ".mymention{ color: gray; }",
+  contextmenu: "link image editimage table configurepermanentpen",
+  a11y_advanced_options: true,
+  skin: useDarkMode ? "oxide-dark" : "oxide",
+  content_css: useDarkMode ? "dark" : "default",
+  /*
+  The following settings require more configuration than shown here.
+  For information on configuring the mentions plugin, see:
+  https://www.tiny.cloud/docs/tinymce/6/mentions/.
+  */
+};
+export default {
+  components: {
+    editor: Editor,
+  },
+  data() {
+    return {
+      config,
+    };
+  },
+};
+</script>
